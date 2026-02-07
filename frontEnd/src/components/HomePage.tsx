b7424dae9a33e560993acb7d4c8916c898b8a213
Rudra-Bhingarde <rudrabhingardedevacc@gmail.com>
2026-02-07 16:00:58 +0530

    animation of the visualize button completed

--- frontEnd/src/components/HomePage.tsx
+++ frontEnd/src/components/HomePage.tsx
@@ -1,22 +1,31 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './HomePage.css'
 
 const HomePage = () => {
   const [visualize, setVisualize] = useState(false)
-
+  const [branch, setBranch] = useState('branch')
+  const handleBranchSelection=(selection: string)=>{
+    if(branch=='branch'){
+      setBranch(selection);
+    }
+    else{
+      setBranch('branch');
+    }
+  }
   return (
     <div className="mainBody">
       <p className="titleName">TensorKit</p>
 
-      <div className="mainContainer">
-        <div
-          className={`selectorContainer ${visualize ? 'withVisualizer' : 'centered'}`}
-        >
-          <button onClick={() => setVisualize(v => !v)}>
-            visualize
-          </button>
+      <div className={`mainContainer ${visualize ? 'visualized' : 'hidden'}`}>
+        <div className={`selectorContainer ${visualize ? 'withVisualizer' : 'centered'}`}>
+          <div className='mlBranches'>
+            {(branch=='branch') || (branch=='Supervised') ?<div className='branchButton' onClick={()=>handleBranchSelection('Supervised')}>Supervised</div>:undefined}
+            {(branch=='branch') || (branch=='unSupervised') ?<div className='branchButton' onClick={()=>handleBranchSelection('unSupervised')}>unSupervised</div>:undefined}
+          </div>
+          <div className='visualizeButton' onClick={() => setVisualize(v => !v)}>
+            {visualize?'Hide':'Visualize'}
+          </div>
         </div>
-
         <div
           className={`visualizerContainer ${visualize ? 'visible' : 'hidden'}`}
         />
